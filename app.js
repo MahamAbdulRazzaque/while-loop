a811dae3c24bff889a35d575bbbdc91edd0cd10f
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-12-09 17:01:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let i = 5;
+while (i <= 10) {
+    document.write(i, "Maham AraB", "<br>");
+    i++;
+}
+
+document.write("<br>");
+
+let j=6;
+do {
+    document.write(j, "Maham AraB", "<br>");
+    j++;
+}
+while (j <= 10);
